f7b12b950dcf333bd13f175cde2465c3a3e39577
root <Ahmed Shaban>
2023-10-05 17:20:05 -0700

    c1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,1 @@
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include<stdio.h>	/**	* main - Entry point	*	* Return: Always 0 (success)	*/	int main(void)	{	puts("\"programming is like building a multilingual puzzle");	return (0);	}
