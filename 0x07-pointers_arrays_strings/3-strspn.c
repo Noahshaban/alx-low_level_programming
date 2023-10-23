935f40b93019b0c4e70157fc180830592615e7a5
Noahshaban <145812990+Noahshaban@users.noreply.github.com>
2023-10-24 01:00:48 +0300

    c8

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,24 +14,24 @@ unsigned int _strspn(char *s, char *accept)
 {
 
 
- unsigned int _strspn(char *s, char *accept);
- unsigned int i = 0;
- int k;
+unsigned int _strspn(char *s, char *accept);
+unsigned int i = 0;
+int k;
 
 
- while (*s)
- {
- for (k = 0; accept[k]; k++)
- {
- if (*s == accept[k])
- {
- i++;
- break;
- }
- else if (accept[k + 1] == '\0')
- return (i);
- }
- s++;
- }
- return (i);
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+{
+i++;
+break;
+}
+else if (accept[k + 1] == '\0')
+return (i);
+}
+s++;
+}
+return (i);
 }
