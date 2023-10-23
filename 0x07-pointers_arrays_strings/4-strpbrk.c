da310f8fd3a05a103c76a709eaeb8896c9b126bf
Noahshaban <145812990+Noahshaban@users.noreply.github.com>
2023-10-24 01:01:38 +0300

    c8

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,16 +14,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
- int k, j;
+int k, j;
 
 
- for (k = 0; s[k] != '\0'; k++)
- {
- for (j = 0; accept[j] != '\0'; j++)
- {
- if (s[k] == accept[j])
- return (s + k);
- }
- }
- return (0);
+for (k = 0; s[k] != '\0'; k++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[k] == accept[j])
+return (s + k);
+}
+}
+return (0);
 }
